f1a52ae7f7cae5fb5450772ab17f8d73e3885229
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-08-29 21:46:46 -0700

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -1,4 +1,4 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
-# Write a program that asks for the user's name and then prints a greeting to them using their input.
+# Write a program that asks for the user's name and another piece of information.Then prints a response using both of the inputs.
